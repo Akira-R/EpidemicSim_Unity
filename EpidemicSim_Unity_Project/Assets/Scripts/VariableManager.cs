91054928c00dff40eb5af932f29c76de862c0c97
ThanathornNilkhao <thanathorn@outlook.com>
2023-03-22 16:16:46 +0700

    + Variable Manager

---
+++ EpidemicSim_Unity_Project/Assets/Scripts/VariableManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using NaughtyAttributes;
+
+public class VariableManager : MonoBehaviour
+{
+    public VariableObject variables;
+
+    public TMP_InputField latitudeInput;
+    public TMP_InputField longitudeInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Initial Variables:");
+        //Debug.Log("Latlng: " + variables.LatlngCoord);
+        //Debug.Log("Population Number: " + variables.PopulationNumber);
+        //Debug.Log("Population Protection: " + variables.PopulationProtection);
+        //Debug.Log("Transmission Rate: " + variables.TransmissionRate);
+        //Debug.Log("Fatalities Rate: " + variables.FatalitiesRate);
+        //Debug.Log("Recovery Rate: " + variables.RecoveryRate);
+        //Debug.Log("Total Sim Time: " + variables.TotalSimulationTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Button]
+    public void UpdateLatlngCoords() 
+    {
+        bool isNumber;
+        float lat;
+        isNumber = float.TryParse(latitudeInput.text.ToString(), out lat);
+        if (!isNumber) 
+        {
+            Debug.Log("Lat is not a number: " + latitudeInput.text.ToString());
+            return;
+        }
+
+        float lng;
+        isNumber = float.TryParse(longitudeInput.text.ToString(), out lng);
+        if (!isNumber) 
+        {
+            Debug.Log("Lng is not a number:" + longitudeInput.text.ToString());
+            return;
+        }
+
+        variables.SetLatlngCoord(lat, lng);
+
+        Debug.Log("Latlng: " + variables.LatlngCoord);
+    } 
+}
